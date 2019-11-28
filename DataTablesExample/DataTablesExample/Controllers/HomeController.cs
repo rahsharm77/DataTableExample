5c357c586a3af041add719557173eb06d6f49b0a
rahsharm77 <43404256+rahsharm77@users.noreply.github.com>
2019-11-28 13:37:44 +0530

    Committing project changes

--- DataTablesExample/DataTablesExample/Controllers/HomeController.cs
+++ DataTablesExample/DataTablesExample/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace DataTablesExample.Controllers
@@ -13,7 +11,6 @@ namespace DataTablesExample.Controllers
         {
             return View();
         }
-
 
         public JsonResult get_date()
         {
@@ -77,6 +74,41 @@ namespace DataTablesExample.Controllers
             });
             var data = listcomp;
             return Json(listcomp, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ExtensionData()
+        {
+            return View();
+        }
+
+        public JsonResult get_extensiondata()
+        {
+            List<ExtensionsData> extensionData = new List<ExtensionsData>();
+
+            //Example of how you could intialize your list
+            extensionData.Add(new ExtensionsData
+            {
+                ExtensionData = { "1", "2" },
+                Code = "40",
+                Name = "Alex"
+            });
+
+            extensionData.Add(new ExtensionsData
+            {
+                ExtensionData = { "1", "2" },
+                Code = "40",
+                Name = "Tom"
+            });
+
+            extensionData.Add(new ExtensionsData
+            {
+                ExtensionData = { "1", "2" },
+                Code = "10",
+                Name = "Bunny"
+            });
+
+            var data = extensionData;
+            return Json(extensionData, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ DataTablesExample/DataTablesExample/Models/ExtensionsData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DataTablesExample
+{
+    public class ExtensionsData
+    {
+        public List<string> ExtensionData=new List<string>(); 
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
